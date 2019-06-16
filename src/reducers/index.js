bcf0fc9fd0aadca24198d2bf504de4a0050ed862
Danielle Quevedo <quevedo.danielle2@gmail.com>
2019-06-16 13:05:13 -0500

    changed spacing of root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,31 +1,31 @@
-import { ADD_LIST_ITEM, DELETE_LIST_ITEM, TOGGLE_COMPLETE, EDIT_LIST_ITEM} from '../actions/types';
+import { ADD_LIST_ITEM, DELETE_LIST_ITEM, TOGGLE_COMPLETE, EDIT_LIST_ITEM } from '../actions/types';
 import initialState from '../data/listItems';
 
-export default function toDoListReducer(state = initialState, action){
-    switch (action.type) {
-        case ADD_LIST_ITEM:
-            return [...state, action.payload];
-        
-        case DELETE_LIST_ITEM: 
-            return state.filter(item => item.id !== action.payload.id);
+export default function toDoListReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_LIST_ITEM:
+      return [...state, action.payload];
 
-        case EDIT_LIST_ITEM:
-            const newEditState = [...state];
-            const editedItem = newEditState.find(e => e.id === action.payload.id); 
-            
-            const editIndex = newEditState.indexOf(editedItem);
-            console.log(editIndex);
-            newEditState[editIndex].isEditing = !newEditState[editIndex].isEditing;
-            return newEditState;
+    case DELETE_LIST_ITEM:
+      return state.filter(item => item.id !== action.payload.id);
 
-        case TOGGLE_COMPLETE:
-            const newToggleState = [...state];
-            const toggledItem = newToggleState.find(e => e.id === action.payload.id); 
-            const index = newToggleState.indexOf(toggledItem);
-            newToggleState[index].complete = !newToggleState[index].complete;
-            return newToggleState;
-                
-        default: 
-            return state;
-    }
+    case EDIT_LIST_ITEM:
+      const newEditState = [...state];
+      const editedItem = newEditState.find(e => e.id === action.payload.id);
+
+      const editIndex = newEditState.indexOf(editedItem);
+      console.log(editIndex);
+      newEditState[editIndex].isEditing = !newEditState[editIndex].isEditing;
+      return newEditState;
+
+    case TOGGLE_COMPLETE:
+      const newToggleState = [...state];
+      const toggledItem = newToggleState.find(e => e.id === action.payload.id);
+      const index = newToggleState.indexOf(toggledItem);
+      newToggleState[index].complete = !newToggleState[index].complete;
+      return newToggleState;
+
+    default:
+      return state;
+  }
 }
