576aea99bb92e5822be274af07aceaf8c1ac634f
Danielle Quevedo <quevedo.danielle2@gmail.com>
2019-06-16 13:28:19 -0500

    displayed ToDoListContainer

--- src/App.js
+++ src/App.js
@@ -4,27 +4,12 @@ import ToDoListContainer from './containers/ToDoListContainer';
 import AddListItem from './containers/AddListItem';
 import NewListItem from './components/NewListItem';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="wrapper">
-//         <AddListItem />
-//         <ToDoListContainer />
-//       </div>
-//     );
-//   }
-// }
-
 function App() {
   return (
     <div className="wrapper">
-    
-    <NewListItem />  
-    {/* <AddListItem /> */}
-    
-    {/* <ToDoListContainer /> */}
-    {/* "YELLO!" */}
-  </div>
+      <NewListItem />
+      <ToDoListContainer />
+    </div>
   );
 }
 
