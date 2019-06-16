9dcc6182ffe09325ec6d436ab9bab58caacd1ac6
Danielle Quevedo <quevedo.danielle2@gmail.com>
2019-06-16 13:10:54 -0500

    added commented explanation to actions/types

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,8 @@
+// the purpose of this file is because if I just use strings 
+// within the actions index and reducers index files, I won't be
+// presented with an error in case of a misspell. But by forcing 
+// myself to import these as variables within those two files,
+// I'll be presented with an error if there is a misspell.
 export const DELETE_LIST_ITEM = "DELETE_LIST_ITEM";
 export const ADD_LIST_ITEM = "ADD_LIST_ITEM";
 export const TOGGLE_COMPLETE = "TOGGLE_COMPLETE";
